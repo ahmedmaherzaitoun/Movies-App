944418fc027a8017557bf64824e59f0fc381f72c
Ahmed Maher <ahmdmahr604@gmail.com>
2022-08-16 01:12:06 +0200

    search keyboard and handle rotation

--- app/src/main/java/com/example/movies/ui/MovieDetailsActivity.kt
+++ app/src/main/java/com/example/movies/ui/MovieDetailsActivity.kt
@@ -22,41 +22,36 @@ class MovieDetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_details)
 
+        // change color of action bar
         val actionBar = supportActionBar
         val colorDrawable = ColorDrawable(Color.parseColor("#eb8f2d"))
         actionBar!!.setBackgroundDrawable(colorDrawable)
 
-        init()
+        initComponent()
 
         var intent = intent
-
         val name = intent.getStringExtra("movieName")
-        val  description = intent.getStringExtra("movieDesc")
-        val  date  = intent.getStringExtra("movieDate")
-        val  posterPath = intent.getStringExtra("moviePoster")
-        val  rate = intent.getStringExtra("movieRate")
+        val description = intent.getStringExtra("movieDesc")
+        val date  = intent.getStringExtra("movieDate")
+        val posterPath = intent.getStringExtra("moviePoster")
+        val rate = intent.getStringExtra("movieRate")
 
         this.setTitle(name)
-
 
         movieName.text = name
         movieDate.text = date
         movieDescription.text = description
         movieRate.text = rate
-
-        moviePoster = findViewById(R.id.movie_poster_details)
-
         Picasso.get().load("https://image.tmdb.org/t/p/w500${posterPath}").resize(200,300).into(moviePoster)
 
     }
-    fun init(){
+    fun initComponent(){
+
         movieName = findViewById(R.id.movie_name_details)
         movieDate = findViewById(R.id.movie_date_details)
         movieDescription = findViewById(R.id.description_details)
         movieRate = findViewById(R.id.rate)
-
         moviePoster = findViewById(R.id.movie_poster_details)
-
 
     }
 }
